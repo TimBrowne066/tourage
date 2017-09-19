73b766de67b1aa58f33a5e228c7516169db31989
Timothy Browne <timbrowne066@gmail.com>
2017-09-19 11:56:10 -0400

    added google user auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918200703) do
+ActiveRecord::Schema.define(version: 20170919141809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,16 @@ ActiveRecord::Schema.define(version: 20170918200703) do
     t.string "venue", null: false
     t.datetime "date", null: false
     t.integer "band_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
+    t.string "oauth_token"
+    t.datetime "oath_expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
